061f6a835741a106686b1d18a8ec64165b8eb100
cycy137 <cycy135@outlook.com>
2026-08-28 13:15:45 -0700

    change optonal

--- spa88/src/pages/StatisticsView.tsx
+++ spa88/src/pages/StatisticsView.tsx
@@ -150,7 +150,7 @@ export default function StatisticsView() {
                     dataKey="value"
                     nameKey="serviceName"
                   >
-                    {serviceChartData.map((entry, index) => (
+                    {serviceChartData.map((_entry, index) => (
                       <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                     ))}
                   </Pie>
